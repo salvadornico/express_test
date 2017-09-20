060ac122d5b31e7202459bd0dac7cb9a11ff235d
Nico Salvador <nicosalvador@creatinginfo.com>
2017-09-20 18:12:48 +0800

    Videos 4 and 5

--- app.js
+++ app.js
@@ -1,26 +1,51 @@
 const express = require("express")
 const path = require("path")
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+
+mongoose.connect("mongodb://localhost/nodekb")
+const db = mongoose.connection
+db.once("open", () => { console.log("Connected to MongoDB") })
+db.on("error", (err) => { alert(err) })
+
 const app = express()
+
+const Article = require("./models/article")
 
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "pug")
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
-	let articles = [
-		{id: 1, title: "Article 1", author: "Me", body: "Hello World"},
-		{id: 2, title: "Article 2", author: "Me Too", body: "Hello World Hello World"},
-		{id: 3, title: "Article 3", author: "Me Three", body: "Hello World Hello World Hello World"},
-	]
-	res.render("index", {
-		title: "Articles",
-		articles: articles
+	Article.find({}, (err, articles) => {
+		if (err) { console.log(err) }
+		res.render("index", {
+			title: "Articles",
+			articles: articles
+		})
 	})
 })
 
 app.get("/articles/add", (req, res) => {
 	res.render("add_article", {
 		title: "Add Article"
+	})
+})
+
+app.post("/articles/add", (req, res) => {
+	let article = new Article()
+	article.title = req.body.title
+	article.author = req.body.author
+	article.body = req.body.body
+
+	article.save((err) => {
+		if (err) {
+			console.log(err)
+		} else {
+			res.redirect("/")
+		}
 	})
 })
 
